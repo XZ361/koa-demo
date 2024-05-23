338a4526bfaf9f9142da4142e1f7bf532e5dd786
XZ361 <2457307263@qq.com>
2024-05-23 14:21:24 +0800

    feat:add 路由分发

--- 01/index.js
+++ 01/index.js
@@ -3,11 +3,18 @@ const app = new Koa();
 
 app.use((ctx) => {
   // console.log(ctx.req.method);
-
   // console.log(ctx.headers);
-  console.log(ctx.method);
-  ctx.body = "hello koa";
+  // console.log(ctx.method);
+  // ctx.body = "hello koa";
   // ctx.res.end("hello koa res");
+  const path = ctx.path;
+  if (path === "/") {
+    ctx.body = "home";
+  } else if (path === "/foo") {
+    ctx.body = "foo 页面";
+  } else {
+    ctx.body = "404 not Found";
+  }
 });
 app.listen(3000, () => {
   console.log("服务启动成功");
