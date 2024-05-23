f24b37f12160039bd2cb75d77827a34d9a8dc4f3
XZ361 <2457307263@qq.com>
2024-05-23 14:15:46 +0800

    feat:add ctx上下文对象

--- 01/index.js
+++ 01/index.js
@@ -2,7 +2,12 @@ const Koa = require("koa");
 const app = new Koa();
 
 app.use((ctx) => {
+  // console.log(ctx.req.method);
+
+  // console.log(ctx.headers);
+  console.log(ctx.method);
   ctx.body = "hello koa";
+  // ctx.res.end("hello koa res");
 });
 app.listen(3000, () => {
   console.log("服务启动成功");
