2c7fba46b7dcf52fe73879876374db1a15cd2d9a
XZ361 <2457307263@qq.com>
2024-05-24 10:58:14 +0800

    feat:add 中间件原理

--- 01/koa/lib/application.js
+++ 01/koa/lib/application.js
@@ -1,11 +1,42 @@
 const http = require("http");
 
 class Application {
+  constructor() {
+    this.middleware = []; //保存用户添加的中间件函数
+  }
   listen(...args) {
-    const serve = http.createServer((req, res) => {
-      res.end("My Koa");
-    });
+    const serve = http.createServer(this.callback());
     serve.listen(...args);
+  }
+  use(fn) {
+    this.middleware.push(fn);
+  }
+  compose(middleware) {
+    // 异步递归遍历调用中间件函数
+    return function () {
+      const dispatch = (index) => {
+        if (index >= middleware.length) return Promise.resolve();
+        const fn = middleware[index];
+        return Promise.resolve(fn({}, () => dispatch(index + 1)));
+      };
+      return dispatch(0);
+    };
+  }
+  callback() {
+    // console.log("111");
+    const fnMiddleware = this.compose(this.middleware);
+    const handleRequest = (req, res) => {
+      fnMiddleware()
+        .then(() => {
+          console.log("end");
+          res.end("hello koa ");
+        })
+        .catch((err) => {
+          res.end(err.message);
+          console.log("err", err);
+        });
+    };
+    return handleRequest;
   }
 }
 module.exports = Application;
